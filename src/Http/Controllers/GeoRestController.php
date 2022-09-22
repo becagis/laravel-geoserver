2971d03857cdb2b845b42bfb84a8629194488af1
levohuutri <levohuutri@gmail.com>
2022-09-22 09:07:43 +0700

    Map Editor API Update to use WFS api

--- src/Http/Controllers/GeoRestController.php
+++ src/Http/Controllers/GeoRestController.php
@@ -30,7 +30,7 @@ class GeoRestController extends BaseController {
     public function list(Request $request, $typeName) {
         return $this->actionVerifyGeonodeToken(function($accessToken) use ($request, $typeName) {
             $validator = Validator::make($request->all(), [
-                'page' => 'integer',
+                'page' => 'integer', 
                 'cql_filter' => 'string'
             ]);
             
