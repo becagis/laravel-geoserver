d8150d5fd6f62cd28ea9e2b6d292df8ea71eca7e
levohuutri <levohuutri@gmail.com>
2022-10-07 13:41:02 +0700

    fix allow admin change permission for all resource

--- src/Http/Traits/PermissionCheckTrait.php
+++ src/Http/Traits/PermissionCheckTrait.php
@@ -16,15 +16,16 @@ trait PermissionCheckTrait {
                 left join auth_permission on auth_permission.id = guardian_userobjectpermission.permission_id
                 left join django_content_type on django_content_type.id = guardian_userobjectpermission.content_type_id
                 join layers_layer on layers_layer.resourcebase_ptr_id::text = guardian_userobjectpermission.object_pk
-                where  (1000 = ? or user_id = ?)  and (object_pk = ? or typename = ?) and codename = ? 
+                where  ((user_id = ?)  and (object_pk = ? or typename = ?) and codename = ?) or (1000 = ?)
                 union 
                 select user_id, codename, model, object_pk from guardian_userobjectpermission 
                 left join auth_permission on auth_permission.id = guardian_userobjectpermission.permission_id
                 left join django_content_type on django_content_type.id = guardian_userobjectpermission.content_type_id
                 join maps_map on maps_map.resourcebase_ptr_id::text = guardian_userobjectpermission.object_pk
-                where (1000 = ? or user_id = ?) and (object_pk = ?) and codename = ?
+                where ((user_id = ?) and (object_pk = ?) and codename = ?) or 1000 = ?
             EOD;
-            $rows = $this->getDbConnection()->select($sql, [$userId, $userId, (string)$pk, (string)$pk, $permCodeName,$userId, $userId, (string)$pk, $permCodeName]);
+            $params = [$userId, (string)$pk, (string)$pk, $permCodeName,$userId, $userId, (string)$pk, $permCodeName, $userId];
+            $rows = $this->getDbConnection()->select($sql, $params);
             return sizeof($rows) > 0;
         } catch (Exception $ex) {
             return false;
